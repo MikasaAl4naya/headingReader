995c77215d8ad3631b6866e373102d59dc6bd059
MikasaAl4naya <oleg.pytin@gmail.com>
2022-11-19 20:46:48 +0800

    Добавили проверку всех файлов в папке Изменили проверку на уникальность элементов списка

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
 import csv
-import datetime
+import datefinder
+import glob
+import os
+import pandas as pd
+for filename in glob.glob('tables\*.csv'):
+   with open(os.path.join(os.getcwd(), filename), 'r') as file:
+        file_reader = csv.reader(file, delimiter=",")
+        row1 = next(file_reader)
+        IncorrectCounter = 0
+        Voidcount = 0
+        print (row1)
+        print('В первой строке в файле', filename)
+        for x in row1:
+            if x == "":
+                Voidcount = Voidcount + 1
+        print("Пустых значений:",Voidcount)
 
-with open("media.csv", encoding='utf-8') as r_file:
-    # Создаем объект reader, указываем символ-разделитель ","
-    file_reader = csv.reader(r_file, delimiter=",")
-    row1 = next(file_reader)
-    dataCounter = 0
-    z = True
-    count = 0
-    for x in row1:
-        if x == "":
-            count = count + 1
-    if count != 0:
-        z = False
-        print("Есть пустые значения")
-    else:
-        print("Пустых значений нет")
-    if z:
-        if len(set(row1)) != len(row1):
-            z = False
-            print("Есть одинаковые значение")
-        else:
-            print("Все значение уникальны")
-
-    if z:
+        out = []
+        repCount= len(row1)-len(set(row1))
+        print("Повторяющихся значений:",repCount)
         for a in row1:
-            d = a
-            if len(d.split('.')) == 3:
-                if datetime.datetime.strptime(d, '%d.%m.%Y'):
-                    dataCounter = +1
-        if dataCounter != 0:
-            z = False
-            print("Есть некорректные значения")
+            # a generator will be returned by the datefinder module. I'm typecasting it to a list. Please read the note of caution provided at the bottom.
+
+            if list(datefinder.find_dates(a)):
+                IncorrectCounter=+1
+            if a.isnumeric():
+                IncorrectCounter=+1
+        print("Некорректных значений", IncorrectCounter)
