6240f037b8ff96ceb9a1bd313ef726f46b983ce7
MikasaAl4naya <oleg.pytin@gmail.com>
2022-11-28 18:08:22 +0800

    Добавили проверку на числа и даты

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import csv
 import datefinder
 import glob
+import re
 import os
-import pandas as pd
 for filename in glob.glob('tables\*.csv'):
    with open(os.path.join(os.getcwd(), filename), 'r') as file:
         file_reader = csv.reader(file, delimiter=",")
         row1 = next(file_reader)
-        IncorrectCounter = 0
-        Voidcount = 0
-        print (row1)
+        IncorrectCounter,IncorCoef,VoidCoef,VoidCount,RepCoef = 0,0,0,0,0
+        print(row1)
         print('В первой строке в файле', filename)
         for x in row1:
             if x == "":
-                Voidcount = Voidcount + 1
-        print("Пустых значений:",Voidcount)
-
+                VoidCount = VoidCount + 1
+                VoidCoef = VoidCount*0.05
+        print("Пустых значений:",VoidCount)
         out = []
-        repCount= len(row1)-len(set(row1))
-        print("Повторяющихся значений:",repCount)
+        RepCount= len(row1)-len(set(row1))
+        RepCoef = RepCount*0.03
+        print("Повторяющихся значений:",RepCount)
         for a in row1:
-            # a generator will be returned by the datefinder module. I'm typecasting it to a list. Please read the note of caution provided at the bottom.
-
-            if list(datefinder.find_dates(a)):
-                IncorrectCounter=+1
-            if a.isnumeric():
-                IncorrectCounter=+1
+            if re.match('\d+', a) or re.match('/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/', a):
+                IncorrectCounter=IncorrectCounter+1
+            IncorCoef=IncorrectCounter*0.02
+        Coef = VoidCoef+ IncorCoef +RepCoef
         print("Некорректных значений", IncorrectCounter)
+        print("Вероятность того, что эта строка заголовок равна", 1 -Coef)
