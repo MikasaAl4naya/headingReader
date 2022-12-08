3c6af9bd5f46b6df203547feb5d010563248ca6e
MikasaAl4naya <oleg.pytin@gmail.com>
2022-12-08 13:41:27 +0800

    Добавили проверку на числа и даты

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ for filename in glob.glob('tables\*.csv'):
    with open(os.path.join(os.getcwd(), filename), 'r') as file:
         file_reader = csv.reader(file, delimiter=",")
         row1 = next(file_reader)
+        w1,w2,w3=2,1,1
         IncorrectCounter,IncorCoef,VoidCoef,VoidCount,RepCoef = 0,0,0,0,0
         print(row1)
         print('В первой строке в файле', filename)
         for x in row1:
             if x == "":
                 VoidCount = VoidCount + 1
-                VoidCoef = VoidCount*0.05
+        VoidCoef = VoidCount*0.05
         print("Пустых значений:",VoidCount)
         out = []
         RepCount= len(row1)-len(set(row1))
@@ -22,7 +23,7 @@ for filename in glob.glob('tables\*.csv'):
         for a in row1:
             if re.match('\d+', a) or re.match('/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/', a):
                 IncorrectCounter=IncorrectCounter+1
-            IncorCoef=IncorrectCounter*0.02
+        IncorCoef=IncorrectCounter*0.02
         Coef = VoidCoef+ IncorCoef +RepCoef
         print("Некорректных значений", IncorrectCounter)
         print("Вероятность того, что эта строка заголовок равна", 1 -Coef)
