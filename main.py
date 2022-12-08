e3d3398d71814124165c140ef278e189632b6e9f
MikasaAl4naya <oleg.pytin@gmail.com>
2022-12-08 18:41:17 +0800

    Добавили проверку на числа и даты

--- main.py
+++ main.py
@@ -3,27 +3,58 @@ import datefinder
 import glob
 import re
 import os
+from collections import Counter
+import dateparser
+def get_unique_cell(row1):
+    count = 0
+    row1 = [x.strip(' ') for x in row1]
+    for x in Counter(row1).values():
+        if x ==1:
+            count+=1
+    return count
+
+
+def get_void_cell(row1):
+    VoidCount = 0
+    row1 = [x.strip(' ') for x in row1]
+    for x in row1:
+        if x == "":
+            VoidCount = VoidCount + 1
+    return VoidCount
+
+
+def get_incorrect_cell(row1):
+    IncorrectCounter = 0
+    row1 = [x.strip(' ') for x in row1]
+    datax = ''
+    for x in row1:
+            datax =dateparser.parse(x)
+            if datax is not None:
+                IncorrectCounter+=1
+            elif re.match(r"([-+]?(?:\d+(?:.\d)?|.\d+)(?:[eE][-+]?\d+)?)", x):
+                IncorrectCounter += 1
+    return IncorrectCounter
+
+
 for filename in glob.glob('tables\*.csv'):
-   with open(os.path.join(os.getcwd(), filename), 'r') as file:
+    with open(os.path.join(os.getcwd(), filename), 'r') as file:
         file_reader = csv.reader(file, delimiter=",")
         row1 = next(file_reader)
-        w1,w2,w3=2,1,1
-        IncorrectCounter,IncorCoef,VoidCoef,VoidCount,RepCoef = 0,0,0,0,0
+        w1, w2, w3 = 2, 1, 1
+        IncorCoef, VoidCoef, RepCoef = 0, 0, 0
         print(row1)
         print('В первой строке в файле', filename)
-        for x in row1:
-            if x == "":
-                VoidCount = VoidCount + 1
-        VoidCoef = VoidCount*0.05
-        print("Пустых значений:",VoidCount)
-        out = []
-        RepCount= len(row1)-len(set(row1))
-        RepCoef = RepCount*0.03
-        print("Повторяющихся значений:",RepCount)
-        for a in row1:
-            if re.match('\d+', a) or re.match('/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/', a):
-                IncorrectCounter=IncorrectCounter+1
-        IncorCoef=IncorrectCounter*0.02
-        Coef = VoidCoef+ IncorCoef +RepCoef
-        print("Некорректных значений", IncorrectCounter)
-        print("Вероятность того, что эта строка заголовок равна", 1 -Coef)
+        print("Всего количества яйчеек:", len(row1))
+        print('Уникальных значений', get_unique_cell(row1))
+        print("Пустых значений:",get_void_cell(row1))
+        print("Некорректных значений", get_incorrect_cell(row1))
+        RepCoef = get_unique_cell(row1)/len(row1) *w1
+        VoidCoef = get_void_cell(row1) / len(row1) * w2
+        IncorCoef=get_incorrect_cell(row1)/len(row1)*w3
+        Coef = RepCoef -(IncorCoef + VoidCoef)
+        if Coef<0:
+            Coef=0
+
+
+        print("Вероятность того, что эта строка заголовок равна", Coef)
+
