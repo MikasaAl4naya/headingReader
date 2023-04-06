cf9807737744ac3d46d10ef66801182edcbe025b
MikasaAl4naya <oleg.pytin@gmail.com>
2023-04-06 19:46:58 +0800

    Последнее обновление

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from collections import Counter
 import pandas as pd
 import dateparser
 import stanza as stanza
+import datetime
+import xlsxwriter
+from openpyxl.workbook import Workbook
 
 
 def get_unique_cell(row1):
@@ -39,31 +42,6 @@ def get_incorrect_cell(row1):
                 IncorrectCounter += 1
     return IncorrectCounter
 
-
-# def aggregate_scores(first_row_score, candidates_scores):
-#     # Нормализуем оценку первой строки
-#     P = first_row_score / len(headers_found)
-#
-#     # Пройдемся по списку предполагаемых кандидатов и добавим оценку P, если индекс первой строки не указан
-#     for candidate in candidates_scores:
-#         if 0 not in candidate[0]:
-#             candidate[0].insert(0, 0)
-#             candidate[1] += P
-#         else:
-#             candidate[1] = P
-#
-#     # Добавим в список новый кандидат, если индекс первой строки не был учтен ранее
-#     new_candidate = [[0], P]
-#     if new_candidate not in candidates_scores:
-#         candidates_scores.append(new_candidate)
-#
-#     # Отсортируем список кандидатов по убыванию оценок
-#     candidates_scores.sort(key=lambda x: x[1], reverse=True)
-#
-#     # Выберем кандидата с максимальной оценкой
-#     AGG = candidates_scores[0][0]
-#
-#     return AGG
 def write_total_score(path,  result):
 
     if not os.path.exists(path):
@@ -115,14 +93,15 @@ def test_ner(text):
     else:
         for ent in doc.ents:
             return ent.type
-
-        # Чтение проверочного CSV-файла
+        from openpyxl import Workbook
+# Создание пустого DataFrame
+results = []
 file = 'test.csv'
 table = pd.DataFrame(pd.read_csv(file))
 stanza.download("ru")
 nlp = stanza.Pipeline(lang="ru", processors="tokenize,ner")
 for filepath in glob.glob('SAUS/*.csv'):
-    with open(os.path.join(os.getcwd(), filepath), 'r') as file:
+    with open(os.path.join(os.getcwd(), filepath), 'r', encoding='cp1251') as file:
         file_reader = csv.reader(file, skipinitialspace=True, delimiter=",")
         reader = list(file_reader)
         rows = 10
@@ -161,13 +140,13 @@ for filepath in glob.glob('SAUS/*.csv'):
                         candidate_scores.append(new_candidate)
         print('candidate_scores:', candidate_scores)
         R=0
+        i=0
         R_list = list()
         filename = os.path.basename(filepath)
         for i in table.index:
-            if table['name'][i] == filename[:-4]:
+            if str(table['name'][i]) == filename[:-4]:
                 R+=1
                 R_list.append(table['id'][i])
-        print(R)
         # Сравнение результатов программы с проверочным файлом
         CH = len(headers_found.intersection(set(range(len(reader[0])))))
         H = len(headers_found)
@@ -184,33 +163,46 @@ for filepath in glob.glob('SAUS/*.csv'):
         print(f"Precision = {precision:.3f}")
         print(f"Recall = {recall:.3f}")
         print(f"F1 = {f1:.3f}")
-        write_total_score('result.txt',filename[:-4]+f', {precision,recall,f1}\n')
-        print("------------------------------------------------------------------------------------------------------------------------")
-        w1, w2, w3 = 2, 1, 1
-        IncorCoef, VoidCoef, RepCoef = 0, 0, 0
-        row1=reader[0]
-        print(row1)
-        print('В первой строке в файле', filename)
-        print("Всего количества яйчеек:", len(row1))
-        print('Уникальных значений', get_unique_cell(row1))
-        print("Пустых значений:",get_void_cell(row1))
-        print("Некорректных значений", get_incorrect_cell(row1))
-        RepCoef = get_unique_cell(row1)/len(row1) *w1
-        VoidCoef = get_void_cell(row1) / len(row1) * w2
-        IncorCoef=get_incorrect_cell(row1)/len(row1)*w3
-        Coef = RepCoef -(IncorCoef + VoidCoef)
-        if Coef<0:
-            Coef=0
-        print("Вероятность того, что первая строка заголовок равна", ((Coef)/2))
-        # Объединение оценок
-        P = Coef
-        for i, candidate in enumerate(candidate_scores):
-            if candidate[0] == [0]:
-                candidate[1] += P
-        # Определение лучшего заголовка
-        best_candidate = max(candidate_scores, key=lambda x: x[1])
-        best_header = reader[best_candidate[0][0]][0]
-        best_score = best_candidate[1]
-        normalized_score = best_score/(3)
-        print('Best header:', best_header)
-        print('Score:', round(normalized_score,2))
+        # Создаем строку для записи в DataFrame
+        row = {'File': os.path.basename(filepath), 'Precision': precision, 'Recall': recall, 'F1': f1}
+        # Добавляем строку в DataFrame
+        # print("------------------------------------------------------------------------------------------------------------------------")
+        # w1, w2, w3 = 2, 1, 1
+        # IncorCoef, VoidCoef, RepCoef = 0, 0, 0
+        # row1=reader[0]
+        # print(row1)
+        # print('В первой строке в файле', filename)
+        # print("Всего количества яйчеек:", len(row1))
+        # print('Уникальных значений', get_unique_cell(row1))
+        # print("Пустых значений:",get_void_cell(row1))
+        # print("Некорректных значений", get_incorrect_cell(row1))
+        # RepCoef = get_unique_cell(row1)/len(row1) *w1
+        # VoidCoef = get_void_cell(row1) / len(row1) * w2
+        # IncorCoef=get_incorrect_cell(row1)/len(row1)*w3
+        # Coef = RepCoef -(IncorCoef + VoidCoef)
+        # if Coef<0:
+        #     Coef=0
+        # print("Вероятность того, что первая строка заголовок равна", ((Coef)/2))
+        # # Объединение оценок
+        # P = Coef
+        # for i, candidate in enumerate(candidate_scores):
+        #     if candidate[0] == [0]:
+        #         candidate[1] += P
+        # # Определение лучшего заголовка
+        # best_candidate = max(candidate_scores, key=lambda x: x[1])
+        # best_header = reader[best_candidate[0][0]][0]
+        # best_score = best_candidate[1]
+        # normalized_score = best_score/(3)
+        # print('Best header:', best_header)
+        # print('Score:', round(normalized_score,2))
+        # Добавление результатов в список
+        results.append({
+            'File': filename,
+            'Recall': recall,
+            'Precision': precision,
+            'F1': f1,
+        })
+
+# Создание DataFrame из списка результатов и сохранение в Excel-файл
+df = pd.DataFrame(results)
+df.to_excel('results.xlsx',sheet_name='Набор данных SAUS', index=False)
